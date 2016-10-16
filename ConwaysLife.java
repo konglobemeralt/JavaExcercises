59c702a796ea1afdf0677e9e9204d98431d1494c
konglobemeralt <jesper.blidkvist@live.se>
2016-10-16 22:25:58 +0200

    Java excercises dump

--- ConwaysLife.java
+++ ConwaysLife.java
@@ -1,12 +1,17 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class ConwaysLife {
 	
+	final int WIDTH = 20;
+	final int HEIGHT= 20;
 	JFrame frame = new JFrame( "Game of life" );
 	MyPanel panel = new MyPanel();
 	
 	public ConwaysLife() {
 		frame.setSize( 600, 600 );
+		frame.setLayout( new BorderLayout());
+		frame.add( panel, BorderLayout.CENTER );
 		
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
 		frame.setVisible( true );
