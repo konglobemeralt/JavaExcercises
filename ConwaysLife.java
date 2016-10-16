22da3b9d76a1db369efad048bafa0012b67c64a0
konglobemeralt <jesper.blidkvist@live.se>
2016-10-16 23:07:34 +0200

    ADDED grid

--- ConwaysLife.java
+++ ConwaysLife.java
@@ -1,23 +1,50 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.*;
 
-public class ConwaysLife {
+public class ConwaysLife implements MouseListener {
 	
-	final int WIDTH = 20;
-	final int HEIGHT= 20;
-	JFrame frame = new JFrame( "Game of life" );
-	MyPanel panel = new MyPanel();
+	final int WIDTH = 20 ;
+	final int HEIGHT= 20 ;
+	JFrame frame = new JFrame( "Game of life" ) ;
+	boolean[][] grid = new boolean [WIDTH] [HEIGHT] ;
+	MyPanel panel = new MyPanel( grid ) ;
 	
 	public ConwaysLife() {
-		frame.setSize( 600, 600 );
-		frame.setLayout( new BorderLayout());
-		frame.add( panel, BorderLayout.CENTER );
+		frame.setSize( 600, 600 ) ;
+		frame.setLayout( new BorderLayout() ) ;
+		frame.add( panel, BorderLayout.CENTER ) ;
 		
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-		frame.setVisible( true );
+		panel.addMouseListener( this );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE) ;
+		frame.setVisible( true ) ;
 	}
 
 	public static void main( String[] args) {
-		new ConwaysLife();		
+		new ConwaysLife() ;		
+	}
+	
+	public void mouseClicked(MouseEvent ev) {
+		
+	}
+	
+	public void mouseEntered(MouseEvent ev) {
+		
+	}
+	
+	public void mouseExited(MouseEvent ev) {
+		
+	}
+	
+	public void mousePressed(MouseEvent ev) {
+		
+	}
+	
+	public void mouseReleased(MouseEvent ev) {
+		System.out.println( ev.getX() + "," + ev.getY()) ;
+		int row = ev.getY() / ( panel.getHeight() / HEIGHT) ;
+		int col = ev.getX() / ( panel.getWidth() / WIDTH );
+		grid[row][col] = !grid[row][col];
 	}
 }
--- MyPanel.java
+++ MyPanel.java
@@ -3,9 +3,26 @@ import java.awt.Graphics;
 
 public class MyPanel extends JPanel {
 	
+	boolean [] [] grid ;
+	
+	public MyPanel( boolean[][] newGrid ) { 
+		grid = newGrid ;
+	}
+	
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.drawLine( 10, 20, 300, 450 );
+		super.paintComponent(g) ;
+		
+		int boxWidth = this.getWidth() / grid.length ;
+		
+		for ( int i = 0 ; i < grid.length+1; i++) {
+			g.drawLine(i * boxWidth, 0 , i * boxWidth, this.getHeight() );
+		}
+		
+		for ( int i = 0 ; i < grid.length+1; i++) {
+			g.drawLine(0, this.getHeight() - (  i * boxWidth ), this.getWidth(), this.getHeight() - (  i * boxWidth ) );
+		}
+		
+		
 		
 	}
 }
